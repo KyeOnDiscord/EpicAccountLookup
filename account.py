d7d5fa298b1e08940de49497da94d9181b673480
Kye <promasterboy@outlook.com>
2024-03-11 16:08:13 +1100

    using pro swapper api + cleanup

--- account.py
+++ account.py
@@ -4,38 +4,30 @@ import urllib.parse
 # xbl, psn, nintendo, steam
 
 
-class ExternalAuth:
+class ExternalAuth:  # pylint: disable=too-few-public-methods
     """ExternalAuth class"""
 
-    def __init__(self, key, value) -> None:
-        self.ServiceName = key
-        self.displayName = "None"
-        self.externalAuthId = "None"
-        self.profileURL = "None"
+    def __init__(self, key, value):
+        self.service_name = key
+        self.display_name = None
+        self.external_auth_id = None
+        self.profile_url = None
         if key in ["xbl", "psn", "steam"]:
-            if value.get("externalDisplayName") != None:
-                self.displayName = value["externalDisplayName"]
+            if value.get("externalDisplayName") is not None:
+                self.display_name = value["externalDisplayName"]
         if key in ["psn", "nintendo", "steam"]:
-            if value.get("externalAuthId") != None:
-                self.externalAuthId = value["externalAuthId"]
+            if value.get("externalAuthId") is not None:
+                self.external_auth_id = value["externalAuthId"]
         if key == "steam":
-            self.profileURL = "http://steamcommunity.com/profiles/" + \
-                urllib.parse.quote(self.externalAuthId)
+            self.profile_url = "http://steamcommunity.com/profiles/" + \
+                urllib.parse.quote(self.external_auth_id)
         if key == "xbl":
-            self.profileURL = "https://account.xbox.com/en-us/profile?gamertag=" + \
-                urllib.parse.quote(self.displayName)
+            self.profile_url = "https://account.xbox.com/en-us/profile?gamertag=" + \
+                urllib.parse.quote(self.display_name)
         if key == "psn":
-            self.profileURL = "https://psnprofiles.com/" + \
-                urllib.parse.quote(self.displayName)
+            self.profile_url = "https://psnprofiles.com/" + \
+                urllib.parse.quote(self.display_name)
 
-    def ToString(self) -> str:
-        return "Service:" + self.ServiceName + " | " + self.profileURL
-
-
-class Account:
-    """Account class"""
-
-    def __init__(self, EpicDisplayName, EpicID, externalAuths) -> None:
-        self.EpicDisplayName = EpicDisplayName
-        self.EpicID = EpicID
-        self.externalAuths = externalAuths
+    def to_string(self) -> str:
+        """Returns a string representation of the ExternalAuth object"""
+        return "Service:" + self.service_name + " | " + self.profile_url
--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-"""Main module for EpicAccountLookup"""
+"""Module for looking up Epic Games Accounts"""
 import re  # For regex
-import auth
-import lookup
+import requests
 
-bearerToken = auth.get_bearer_token()
-inputIDorName = input("Enter Epic Account ID: ")
-if re.search(r'^[0-9a-f]{32}$', inputIDorName):
-    acc = lookup.lookup_account_by_id(bearerToken, inputIDorName)
+from account import ExternalAuth
+
+URL = "https://api.proswapper.xyz"
+
+id_or_name = input("Enter Epic Account ID or Display Name: ")
+
+url = f"{URL}/external/name/{id_or_name}"  # URL for searching by Display Name
+if re.search(r'^[0-9a-f]{32}$', id_or_name):  # Checks if the input is an ID
+    url = f"{URL}/external/id/{id_or_name}"
+
+response = requests.request("GET", url, timeout=10)
+result = response.json()
+if len(result) == 0:
+    print("No account found")
 else:
-    print("Invalid Epic Account ID, it should look like this: 0123456789abcdef0123456789abcdef")
-
-# Not yet implemented
-# requires a bearer token with 'account:public:account READ' permission
-# https://github.com/LeleDerGrasshalmi/FortniteEndpointsDocumentation/blob/main/EpicGames/AccountService/Account/Lookup/Displayname.md
-# lookup.LookupAccountByName(bearerToken, input)
+    if len(result[0]["externalAuths"].items()) == 0:
+        print("No external auths found")
+    else:
+        for key, value in result[0]["externalAuths"].items():
+            print(ExternalAuth(key, value).to_string())
